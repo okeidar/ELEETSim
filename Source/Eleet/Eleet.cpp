3502f007468cfc7b427bf42a40b1174c099d561f
okeidar <33117174+okeidar@users.noreply.github.com>
2022-12-01 13:48:44 +0200

    initial commit

---
+++ Source/Eleet/Eleet.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Eleet.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Eleet, "Eleet" );
+ 
